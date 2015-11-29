2ca6e8927dff226553264e94af2368ee9c3f6313
Pedro <Pedro@Pedro-PC>
2015-11-29 14:16:50 -0200

    bd

--- Av3LabBD/Sql/Query.sql
+++ Av3LabBD/Sql/Query.sql
@@ -8,3 +8,7 @@ SELECT * FROM sys.sysdatabases
 
 
 SELECT dbid,name,crdate FROM sys.sysdatabases where name not like 'tempdb'
+
+
+SELECT dbid,name,convert(varchar(10),crdate, 103) as data, 
+	substring(convert(varchar(10),crdate, 108),1,5) as hora FROM sys.sysdatabases where name not like 'tempdb'
