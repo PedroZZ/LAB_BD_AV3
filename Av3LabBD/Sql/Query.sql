6e9588c9b07ca18dbb040fb92ab484abde0b94a7
Pedro <Pedro@Pedro-PC>
2015-11-29 16:31:35 -0200

    BANCO

--- Av3LabBD/Sql/Query.sql
+++ Av3LabBD/Sql/Query.sql
@@ -12,3 +12,30 @@ SELECT dbid,name,crdate FROM sys.sysdatabases where name not like 'tempdb'
 
 SELECT dbid,name,convert(varchar(10),crdate, 103) as data, 
 	substring(convert(varchar(10),crdate, 108),1,5) as hora FROM sys.sysdatabases where name not like 'tempdb'
+	
+
+
+--TOPICO 2
+create procedure sp_backupBD(@nome varchar(100))
+as
+declare @pasta varchar(100), @bak varchar(4), @caminho varchar(max)
+declare c_backup cursor for
+select name FROM sys.sysdatabases where name not like 'tempdb'
+open c_backup
+fetch next from c_backup into @nome
+set @pasta = 'C:\Users\Pedro\Documents\'
+set @bak = '.bak'
+
+while @@fetch_status=0
+begin
+	set @caminho = @pasta + @nome + @bak
+	backup database @nome  
+		to disk=@caminho
+	
+	fetch next from c_backup into @nome
+end
+close c_backup
+deallocate c_backup
+
+
+exec sp_backupBD 'ecommerce'
